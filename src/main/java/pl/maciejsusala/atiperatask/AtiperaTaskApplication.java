21d8c4b4935ad90f194de931450201bb5c5bb2c6
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-08-10 18:11:21 +0200

    initial commit

---
+++ src/main/java/pl/maciejsusala/atiperatask/AtiperaTaskApplication.java
@@ -0,0 +1,15 @@
+package pl.maciejsusala.atiperatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class AtiperaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaTaskApplication.class, args);
+	}
+
+}
