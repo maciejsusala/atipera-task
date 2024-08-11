f82348e45bda152439b5d878eb4bcf5f4b71131d
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-08-11 12:31:18 +0200

    added custom exception message for error 404

--- src/main/java/pl/maciejsusala/atiperatask/config/CustomFeignErrorDecoder.java
+++ src/main/java/pl/maciejsusala/atiperatask/config/CustomFeignErrorDecoder.java
@@ -8,6 +8,9 @@ public class CustomFeignErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
+        if (response.status() == 404) {
+            return new CustomFeignException(response.status(), "Given user don't exist");
+        }
         return new CustomFeignException(response.status(), response.reason());
     }
 }
